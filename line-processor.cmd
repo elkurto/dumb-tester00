c935711d2b43df57a6dabdbf76ba440df2ae2738
elkurto <kurt.buehner@gmail.com>
2025-10-02 05:48:20 -0400

    added line-process.cmd : comments

--- line-processor.cmd
+++ line-processor.cmd
@@ -6,3 +6,6 @@ for /f "userbackq delims=" %%A in (%INFILE%) do (
     echo %%A >> foo.txt
 )
 
+
+REM - The above idiom does note process trailing newlines.
+REM - strangely one can use the above idiom to remove trailing newlines
